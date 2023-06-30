b4b79455c0ff6357afd72df9d1ce9c5394a51b41
AtilaRocha <atila.o.rocha@gmail.com>
2023-06-30 18:33:31 -0300

    Criando Templates, models e views

--- core/models.py
+++ core/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    preco = models.DecimalField('Preco')
+    estoque = models.IntegerField('Quantidade em Estoque')
